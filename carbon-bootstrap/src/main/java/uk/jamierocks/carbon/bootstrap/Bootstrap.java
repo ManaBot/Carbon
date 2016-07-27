fb156ede1b2f5fad319760f7f84dccb236a75f31
Jamie Mansfield <dev@jamierocks.uk>
2016-07-27 12:53:25 +0100

    Download all the dependencies

--- carbon-bootstrap/src/main/java/uk/jamierocks/carbon/bootstrap/Bootstrap.java
+++ carbon-bootstrap/src/main/java/uk/jamierocks/carbon/bootstrap/Bootstrap.java
@@ -24,14 +24,44 @@
 
 package uk.jamierocks.carbon.bootstrap;
 
+import uk.jamierocks.carbon.bootstrap.util.BootstrapConstants;
 import uk.jamierocks.mana.carbon.Main;
 
 /**
  * The bootstrap entry-point.
+ *
+ * @author Jamie Mansifield
+ * @since 2.0.0
  */
 public final class Bootstrap {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        final DependencyManager dependencyManager = new DependencyManager(BootstrapConstants.LIBRARIES_PATH);
+
+        // Basic dependencies
+        dependencyManager.checkDependency(BootstrapConstants.MAVEN_CENTRAL, "com/google/guava/guava/19.0/guava-19.0.jar");
+        dependencyManager.checkDependency(BootstrapConstants.MAVEN_CENTRAL, "com/google/inject/guice/4.0/guice-4.0.jar");
+
+        // Configurate dependencies
+        dependencyManager.checkDependency(BootstrapConstants.MAVEN_CENTRAL,
+                "ninja/leaping/configurate/configurate-core/3.1.1/configurate-core-3.1.1.jar");
+        dependencyManager.checkDependency(BootstrapConstants.MAVEN_CENTRAL,
+                "ninja/leaping/configurate/configurate-hocon/3.1.1/configurate-hocon-3.1.1.jar");
+
+        // Logging dependencies
+        dependencyManager.checkDependency(BootstrapConstants.MAVEN_CENTRAL, "org/slf4j/slf4j-api/1.7.21/slf4j-api-1.7.21.jar");
+        dependencyManager.checkDependency(BootstrapConstants.MAVEN_CENTRAL, "org/apache/logging/log4j/log4j-api/2.6.1/log4j-api-2.6.1.jar");
+        dependencyManager.checkDependency(BootstrapConstants.MAVEN_CENTRAL, "org/apache/logging/log4j/log4j-core/2.6.1/log4j-core-2.6.1.jar");
+        dependencyManager.checkDependency(BootstrapConstants.MAVEN_CENTRAL,
+                "org/apache/logging/log4j/log4j-slf4j-impl/2.6.1/log4j-slf4j-impl-2.6.1.jar");
+
+        // IRC dependencies
+        dependencyManager.checkDependency(BootstrapConstants.MAVEN_CENTRAL, "org/kitteh/irc/client-lib/1.1.0/client-lib-1.1.0.jar");
+
+        // Command dependencies
+        dependencyManager.checkDependency(BootstrapConstants.MISERABLE_REPO,
+                "com/sk89q/intake/intake/4.2-MISNIN-SNAPSHOT/intake-4.2-MISNIN-20160413.183647-1.jar");
+
         Main.main(args);
     }
 }
